b4aac5fd3f9b4776da8845d2b82280040a26e962
Grace Haza <gracehaza@Graces-MacBook-Pro.local>
2020-12-05 01:01:12 +0100

    day 04 part 1 is off by 2 (248 instead of 250)

---
+++ day04/day04a.py
@@ -0,0 +1,50 @@
+
+filepath = 'day04testinput.txt'
+
+def look_at_passport(begin,end):
+    with open(filepath,"r") as filenow:
+        lineslist=filenow.readlines()
+        requirements=['byr:','iyr:','eyr:','hgt:','hcl:','ecl:','pid:']
+        validity=[]
+        for k in range(0, len(requirements)):
+            valid = False
+            for j in range(begin,end):
+                line=lineslist[j]
+                bool = line.find(requirements[k])
+                if bool != -1:
+                    valid = True
+            validity.append(valid)
+        return validity
+        
+def main():
+    with open(filepath,"r") as filenow:
+        totalpassportcount = 0
+        invalidpassportcount = 0
+        lineslist = filenow.readlines()
+        min = 0
+        for i in range(1,len(lineslist)):
+            line = lineslist[i]
+            if line =="\n":
+                max = i
+                validityarray = look_at_passport(min, max)
+                totalpassportcount +=1
+                for l in range(0,len(validityarray)):
+                    if validityarray[l] == False:
+                        invalidpassportcount += 1
+                min = i
+
+            if i == len(lineslist)-1:
+                lastvalidityarray = look_at_passport(max, len(lineslist))
+                totalpassportcount +=1
+                for m in range (0,len(lastvalidityarray)):
+                    if lastvalidityarray[m] == False:
+                        invalidpassportcount += 1
+
+        print 'total passports invalid: ',invalidpassportcount
+    validpassportnumber = totalpassportcount - invalidpassportcount
+    print 'total passports" ', totalpassportcount
+    print 'total valid passport: ', validpassportnumber
+
+
+if __name__ == "__main__":
+    main()
